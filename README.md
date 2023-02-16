ef26d76f89ef3cd9ba58ab750f19bb1ff1ff04b8
andersmateusz <anders_mateusz@outlook.com>
2023-02-16 22:35:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# AndmExampleShopwarePlugin1
---
+++ src/AndmExamplePlugin1.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace AndmExamplePlugin1;
+
+use Shopware\Core\Framework\Plugin;
+
+class AndmExamplePlugin1 extends Plugin
+{
+}
